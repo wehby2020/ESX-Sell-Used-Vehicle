6a488c524408b75b27de1d05a5b81787bc72a24f
wehby2020 <96753451+wehby2020@users.noreply.github.com>
2024-09-07 16:35:07 +0000

    Update vehicles_for_sale.sql

--- vehicles_for_sale.sql
+++ vehicles_for_sale.sql
@@ -2,11 +2,17 @@ CREATE TABLE `vehicles_for_sale` (
   `id` int(11) NOT NULL,
   `seller` varchar(50) NOT NULL,
   `vehicleProps` longtext NOT NULL,
-  `price` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `price` int(11) NOT NULL DEFAULT 0,
+  `description` varchar(200) DEFAULT NULL,
+  `plate` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
-ALTER TABLE `vehicles_for_sale` MODIFY COLUMN `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE `vehicles_for_sale` ADD COLUMN `plate` varchar(15) NOT NULL;
+ALTER TABLE `vehicles_for_sale`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `vehicles_for_salew`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 CREATE TABLE IF NOT EXISTS `carmileages` (
   `plate` text DEFAULT NULL,
